d94b66e9d5a6c9da8db998c998ed7f66cc5f5be3
ft_exhell <exhell@student.21-school.ru>
2021-03-20 04:39:21 +0300

    compiled_contract

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -11,7 +11,7 @@ contract MyNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("MyNFT", "NFT") {}
+    constructor() public ERC721("AztecNFT", "AZTC") {}
 
     function mintNFT(address recipient, string memory tokenURI)
         public onlyOwner
